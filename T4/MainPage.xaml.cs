89b420cfbd1d354f38238fcc90f2a379e9c871cd
H8872 <H8872@student.jamk.fi>
2016-02-11 16:52:08 +0200

    T4 finished.

--- T4/MainPage.xaml.cs
+++ T4/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +23,86 @@ namespace T4
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        string result, buttonString;
+        double num;
+        Size minSize = new Size(800, 500);
         public MainPage()
         {
             this.InitializeComponent();
+
+            ApplicationView.PreferredLaunchViewSize = minSize;
+            ApplicationView.PreferredLaunchWindowingMode
+                = ApplicationViewWindowingMode.PreferredLaunchViewSize;          
+        }
+
+        //public void SetPreferredMinSize(Size minSize)
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            result = resultTextBox.Text;
+            if(result == "0")
+            {
+                result = result.Substring(0, result.Length - 1);
+            }
+            buttonString = (((Button)sender).Content).ToString();
+            result += buttonString;
+            resultTextBox.Text = result;
+        }
+
+        private void commaButton_Click(object sender, RoutedEventArgs e)
+        {
+            result = resultTextBox.Text;
+            result += ".";
+            resultTextBox.Text = result;
+        }
+
+        private void enterButton_Click(object sender, RoutedEventArgs e)
+        {
+            result = resultTextBox.Text;
+            Double.TryParse(result, out num);
+            if ((bool)tempRadioButton.IsChecked)
+            {
+                if (num > 100)
+                {
+                    errorTextBlock.Text = "Error: Temperature too high!";
+                }
+                else if (num < 0)
+                {
+                    errorTextBlock.Text = "Error: Temperature too low!";
+                }
+                else
+                {
+                    tempTextBlock.Text = num.ToString("0.00");
+                    errorTextBlock.Text = "";
+                }
+            }
+            else
+            {
+                if (num > 120)
+                {
+                    errorTextBlock.Text = "Error: Humidity too high!";
+                }
+                else if (num < 0)
+                {
+                    errorTextBlock.Text = "Error: Humidity too low!";
+                }
+                else
+                {
+                    humiTextBlock.Text = num.ToString("0.00");
+                    errorTextBlock.Text = "";
+                }
+            }
+            resultTextBox.Text = "0";
+        }
+        private void exButton_Click(object sender, RoutedEventArgs e)
+        {
+            result = resultTextBox.Text;
+            result = result.Substring(0, result.Length - 1);
+            if(result == "")
+            {
+                result = "0";
+            }
+            resultTextBox.Text = result;
         }
     }
 }
